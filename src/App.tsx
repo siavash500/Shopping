360b06915fbbe71427d1614ca3b56ac580091dfe
siavash500 <siavash.yotube1387@example.com>
2025-07-11 04:20:13 -0700

    i create the first projects whit vite and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './style.css';
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-600 text-center mt-10">
+        Tailwind داره کار می‌کنه!
+      </h1>
+    </>
+  )
+}
+
+export default App
